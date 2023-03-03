5b05f5576a57419c589a92c42913979da3c2f60e
Divya Gupta <divygupta0319@gmail.com>
2023-03-03 20:19:29 +0530

    Initial commit after resolving pom.xml error and updating server port to 8100

---
+++ src/test/java/com/in28Minutes/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28Minutes.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
